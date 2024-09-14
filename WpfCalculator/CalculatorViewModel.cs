dac245b6e1c14507e8d2abcbad2a4c484c29b042
Restsofts <restsofts@gmail.com>
2024-09-14 13:21:17 +0100

    Add files via upload

---
+++ WpfCalculator/CalculatorViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfCalculator
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private string _displayText = "";
+        public string DisplayText
+        {
+            get => _displayText;
+            set
+            {
+                _displayText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand NumberCommand { get; }
+        public ICommand OperationCommand { get; }
+        public ICommand CalculateCommand { get; }
+
+        public CalculatorViewModel()
+        {
+            NumberCommand = new CustomCommand(ExecuteNumber);
+            OperationCommand = new CustomCommand(ExecuteOperation);
+            CalculateCommand = new CustomCommand(ExecuteCalculate);
+        }
+
+        private void ExecuteNumber(object parameter)
+        {
+            DisplayText += parameter.ToString();
+        }
+
+        private void ExecuteOperation(object parameter)
+        {
+            DisplayText += " " + parameter.ToString() + " ";
+        }
+
+        private void ExecuteCalculate(object parameter)
+        {
+            try
+            {
+                var result = new DataTable().Compute(DisplayText, null);
+                DisplayText = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                // تخصيص رسالة الخطأ للمستخدم
+                MessageBox.Show("حدث خطأ أثناء الحساب: " + ex.Message);
+                DisplayText = "خطأ";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfCalculator/CustomCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace WpfCalculator
+{
+    public class CustomCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public CustomCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
